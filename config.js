1ba01e5249130dac0b5403dc2c11809442ebc770
veltardk <dzmitryk.dev@gmail.com>
2016-01-27 06:03:55 +0000

    use configurable pin numbers

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var config = {};
 
-config.DAY_LIGHT_PIN = 7;
-config.NIGHT_LIGHT_PIN = 11;
-config.BLA_1_PIN = 13;
-config.BLA_2_PIN = 14;
+config.DAY_LIGHT_PIN = 11;
+config.NIGHT_LIGHT_PIN = 13;
+config.BLA_1_PIN = 15;
+config.BLA_2_PIN = 16;
 
 config.RELAY_ON = 0;
 config.RELAY_OFF = 1;
