738fa23869ae94bb59661bbebbe94a0d0dae794b
veltardk <dzmitryk.dev@gmail.com>
2016-01-31 20:54:04 +0000

    connect post for controller

--- public/core.js
+++ public/core.js
@@ -1,58 +1,74 @@
 var app = angular.module('FishTankApp', ['ngRoute']);
 
 
-	// configure our routes
-	app.config(function($routeProvider) {
-		$routeProvider
+// configure our routes
+app.config(function($routeProvider) {
+	$routeProvider
 
-			// route for the home page
-			.when('/', {
-				templateUrl : 'pages/home.html',
-				controller  : 'mainController'
-			})
-			// route for the home page
-			.when('/home', {
-				templateUrl : 'pages/home.html',
-				controller  : 'mainController'
-			})
+	// route for the home page
+		.when('/', {
+			templateUrl: 'pages/home.html',
+			controller: 'mainController'
+		})
+		// route for the home page
+		.when('/home', {
+			templateUrl: 'pages/home.html',
+			controller: 'mainController'
+		})
 
-			// route for the about page
-			.when('/about', {
-				templateUrl : 'pages/about.html',
-				controller  : 'aboutController'
-			})
+	// route for the about page
+	.when('/about', {
+		templateUrl: 'pages/about.html',
+		controller: 'aboutController'
+	})
 
-			// route for the setings page
-			.when('/setings', {
-				templateUrl : 'pages/setings.html',
-				controller  : 'setingsController'
-			})
-			
-			// route for the controls page
-			.when('/controls', {
-				templateUrl : 'pages/controls.html',
-				controller  : 'controlsController'
-			});
+	// route for the setings page
+	.when('/setings', {
+		templateUrl: 'pages/setings.html',
+		controller: 'setingsController'
+	})
+
+	// route for the controls page
+	.when('/controls', {
+		templateUrl: 'pages/controls.html',
+		controller: 'controlsController'
 	});
+});
 
-	// create the controller and inject Angular's $scope
-	app.controller('mainController', function($scope) {
-		// create a message to display in our view
-		$scope.message = 'Everyone come and see how good I look!';
-	});
+// create the controller and inject Angular's $scope
+app.controller('mainController', function($scope) {
+	// create a message to display in our view
+	$scope.message = 'Everyone come and see how good I look!';
+});
 
-	app.controller('aboutController', function($scope) {
-		$scope.message = 'About! I am an about page.';
-	});
+app.controller('aboutController', function($scope) {
+	$scope.message = 'About! I am an about page.';
+});
 
-	app.controller('controlsController', function($scope) {
-		$scope.message = 'Controls. This is just a demo.';
-	});
-	
-		app.controller('setingsController', function($scope) {
-		$scope.message = 'setings bla bla .just a demo.';
-	});
-	
+app.controller('controlsController', function($scope) {
+	$scope.message = 'Controls. This is just a demo.';
+	// // post
+	function post(url) {
+		var request = new XMLHttpRequest();
+		request.open('POST', url, true);
+		request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+		request.send({});
+	}
+
+	$scope.lightON = function() {
+		post("/api/light/day/on");
+	};
+
+	$scope.lightOFF = function() {
+		post("/api/light/day/off");
+	};
+
+});
+
+app.controller('setingsController', function($scope) {
+	$scope.message = 'setings bla bla .just a demo.';
+});
+
 
 // app.controller("mainController", function ($scope) {
 
@@ -84,20 +100,20 @@ var app = angular.module('FishTankApp', ['ngRoute']);
 
 
 // // post
-//  function post(url)
-//  {
-//  	var request = new XMLHttpRequest();
-//  	request.open('POST', url, true);
-//  	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-//  	request.send({});
-//  }
+//function post(url)
+//{
+//	var request = new XMLHttpRequest();
+//	request.open('POST', url, true);
+//	request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+//	request.send({});
+//}
 
-//   $scope.lightON = function() {
-//   post("/api/light/day/on");
-//   };
-  
-//   $scope.lightOFF = function() {
-//   post("/api/light/day/off");
-//   };
-  
+// $scope.lightON = function() {
+// post("/api/light/day/on");
+// };
+
+// $scope.lightOFF = function() {
+// post("/api/light/day/off");
+// };
+
 // });
